13f7dec0fbb433c7a2cd0a4247bb8c5170c8081b
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2014-05-08 10:30:25 +0200

    Initial Commit

---
+++ SchemeTestTests/SchemeTestTests.m
@@ -0,0 +1,34 @@
+//
+//  SchemeTestTests.m
+//  SchemeTestTests
+//
+//  Created by Fabian Ehrentraud on 08.05.14.
+//  Copyright (c) 2014 my.org. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SchemeTestTests : XCTestCase
+
+@end
+
+@implementation SchemeTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
